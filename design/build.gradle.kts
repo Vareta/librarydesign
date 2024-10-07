a0199362762e1e94066bb33944c62ee6b1978be7
Bustamante Fuentes, Alvaro <alvaro.bustamantefuentes@cencosud.cl>
2024-10-07 17:01:59 -0300

    Update version

--- design/build.gradle.kts
+++ design/build.gradle.kts
@@ -83,7 +83,7 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.github.Vareta"
                 artifactId = "librarydesign"
-                version = "0.6"
+                version = "0.7"
                 afterEvaluate {
                     from(components["release"])
                 }
