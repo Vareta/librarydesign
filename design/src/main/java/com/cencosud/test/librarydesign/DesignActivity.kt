d32ca83605fdf7ecf602a9794f61e21623514a3c
Bustamante Fuentes, Alvaro <alvaro.bustamantefuentes@cencosud.cl>
2024-10-07 13:10:56 -0300

    Change text color

--- design/src/main/java/com/cencosud/test/librarydesign/DesignActivity.kt
+++ design/src/main/java/com/cencosud/test/librarydesign/DesignActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cencosud.test.core.TestButton
@@ -21,7 +23,8 @@ class DesignActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LibrarydesignTheme {
-                Column(modifier = Modifier.fillMaxSize()) {
+                Column(modifier = Modifier
+                    .fillMaxSize()) {
                     Content()
                 }
             }
@@ -33,9 +36,11 @@ class DesignActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
+        color = Color.Blue,
         modifier = modifier
     )
 }
+
 @Composable
 @Preview
 fun Content() {
